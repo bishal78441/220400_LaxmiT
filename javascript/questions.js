9e8105036d97f93f4961bee00fc038b5368c928e
bishal78441 <timsinabishal232@gmail.com>
2023-05-18 10:13:14 +0545

    questions

---
+++ javascript/questions.js
@@ -0,0 +1,67 @@
+//Question1
+// var userInput = prompt("Enter a number:"); // Assuming user input is taken via prompt
+
+// if (isNaN(userInput)) {
+//   console.log("Invalid input. Please enter a number.");
+// } else {
+//   var number = Number(userInput);
+//   if (number > 10) {
+//     console.log("Wow, that's a big number!");
+//   } else {
+//     console.log("Number is so small.");
+//   }
+// }
+
+
+//question2
+// var score = prompt("Enter your test score:");
+// var grade;
+
+// if (score >= 90 && score <= 100) {
+//   grade = "A";
+// } else if (score >= 80 && score <= 89) {
+//   grade = "B";
+// } else if (score >= 70 && score <= 79) {
+//   grade = "C";
+// } else if (score >= 60 && score <= 69) {
+//   grade = "D";
+// } else if (score < 60) {
+//   grade = "F";
+// } else {
+//   grade = "Invalid score. Please enter a number between 0 and 100.";
+// }
+
+// console.log("Your grade is: " + grade);
+
+
+//factorialll
+// function calculateFactorial(number) {
+//   if (number < 0) {
+//     return "Factorial is not defined for negative numbers.";
+//   } else if (number === 0 || number === 1) {
+//     return 1;
+//   } else {
+//     let factorial = 1;
+//     for (let i = 2; i <= number; i++) {
+//       factorial *= i;
+//     }
+//     return factorial;
+//   }
+// }
+
+// // Prompt the user to input a number
+// const input = prompt("Enter a number:");
+// const number = parseInt(input);
+
+// // Check if the input is a valid number
+// if (isNaN(number)) {
+//   console.log("Invalid input. Please enter a valid number.");
+// } else {
+//   // Calculate the factorial and display the result
+//   const factorial = calculateFactorial(number);
+//   console.log(`The factorial of ${number} is: ${factorial}`);
+// }
+
+
+
+//quiz
